22bf35a344c3359f687dc9bc60b48b47fba602a1
23aneessss <oa_bouziani@esi.dz>
2026-03-28 19:43:41 +0100

    Initial Commit

---
+++ FocusTime/FocusTimeApp.swift
@@ -0,0 +1,17 @@
+//
+//  FocusTimeApp.swift
+//  FocusTime
+//
+//  Created by mac on 28/3/2026.
+//
+
+import SwiftUI
+
+@main
+struct FocusTimeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
